618e78c5154006a3e8fb4ee32a4e586adf2a8dde
AlonTzafari <77238176+AlonTzafari@users.noreply.github.com>
2024-12-17 05:20:03 +0200

    deploy sync

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,4 +3,5 @@ echo "$ALONTZ_DEV_KEY" > key.pem
 chmod 400 key.pem
 scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i key.pem ./deployment/docker-compose.yaml admin@$ALONTZ_HOST:/home/admin/chat/docker-compose.yaml
 ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i key.pem admin@$ALONTZ_HOST 'docker compose -f /home/admin/chat/docker-compose.yaml up -d'
+ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i key.pem admin@$ALONTZ_HOST 'docker compose -f /home/admin/chat/docker-compose.yaml restart'
 rm key.pem
