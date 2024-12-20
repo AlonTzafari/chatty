f931349e2ae363624182e8d2a5765bc9512af2dd
AlonTzafari <77238176+AlonTzafari@users.noreply.github.com>
2024-12-20 16:08:39 +0200

    self hosted pg

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,6 +2,6 @@
 echo "$ALONTZ_DEV_KEY" > key.pem
 chmod 400 key.pem
 scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i key.pem ./deployment/docker-compose.yaml admin@$ALONTZ_HOST:/home/admin/chat/docker-compose.yaml
-ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i key.pem admin@$ALONTZ_HOST 'docker compose -f /home/admin/chat/docker-compose.yaml up -d'
-ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i key.pem admin@$ALONTZ_HOST 'docker compose -f /home/admin/chat/docker-compose.yaml restart'
+ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i key.pem admin@$ALONTZ_HOST 'docker compose -f /home/admin/chat/docker-compose.yaml --profile=infra up -d'
+ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i key.pem admin@$ALONTZ_HOST 'docker compose -f /home/admin/chat/docker-compose.yaml --profile=infra restart'
 rm key.pem
